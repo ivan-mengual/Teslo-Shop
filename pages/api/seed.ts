f03bd9674a7d4600bcf4f497a5d52ae17c86f0ce
Ivan <imengual@itechglobal.solutions>
2023-06-11 19:41:16 +0200

    eliminate mongo carpet

--- pages/api/seed.ts
+++ pages/api/seed.ts
@@ -1,5 +1,5 @@
 import { db, seedDatabase } from '@/database'
-import { Product, User } from '@/models'
+import { Order, Product, User } from '@/models'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {message: string}
@@ -18,6 +18,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
   await Product.deleteMany()
   await Product.insertMany(seedDatabase.initialData.products)
 
+  await Order.deleteMany()
   await db.disconnect()
   
   res.status(200).json({ message: 'Proceso realizado correctamente' })
